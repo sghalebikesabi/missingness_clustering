867c2b452782bea9804406898d1c1e921384bdf8
Sahra Ghalebikesabi <s.ghalebikesabi@gmail.com>
2020-03-27 14:14:37 +0000

    allow different clusters within one batch

--- src/vae.py
+++ src/vae.py
@@ -10,6 +10,8 @@ from torchvision.utils import save_image
 
 
 parser = argparse.ArgumentParser(description='VAE MNIST Example')
+parser.add_argument('--k', type=int, default=2, metavar='N',
+                    help='number of clusters')
 parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                     help='input batch size for training (default: 128)')
 parser.add_argument('--epochs', type=int, default=10, metavar='N',
@@ -36,18 +38,15 @@ test_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
     batch_size=args.batch_size, shuffle=True, **kwargs)
 
-k = 1
 n_train = len(train_loader)*args.batch_size
 n_test = len(test_loader)*args.batch_size
-pC = np.random.uniform(size=k)
+pC = np.random.uniform(size=args.k)
 pC = pC/np.sum(pC)
-#C_train = torch.from_numpy(np.random.choice(np.arange(k), n_train, list(pC)))
-#C_test = torch.from_numpy(np.random.choice(np.arange(k), n_test, list(pC)))
-C_train = torch.tensor(np.zeros((n_train)))
-C_test = torch.tensor(np.zeros((n_train)))
+C_train = torch.from_numpy(np.random.choice(np.arange(args.k), n_train, list(pC)))
+C_test = torch.from_numpy(np.random.choice(np.arange(args.k), n_test, list(pC)))
 
 class VAE(nn.Module):
-    def __init__(self, input_dim, k=1, distinct_hdim=[[400]], commonencoder_hdim=[[20,20]], decoder_hdim=[400]):
+    def __init__(self, input_dim, k=2, distinct_hdim=[[400],[400]], commonencoder_hdim=[[20,20]], decoder_hdim=[400]):
         super(VAE, self).__init__()
         
         # create attributes needed for the encoder
@@ -80,15 +79,20 @@ class VAE(nn.Module):
 
     def encode(self, x, C):
         # create list of list of tensors for saving results of each distinct layer in a list 
-        y = [[x[C==l,:] for l in range(self.k)]]
-
+        y = [[x[C==l,:]] for l in range(self.k) if len(x[C==l,:])>0]
+        y_C = [l for l in range(self.k) if len(x[C==l,:])>0]
         z = []
-        for i, l in enumerate(self.layers):
+        for i, m in enumerate(self.layers):
             # distinct encoder layers
-            if ((i//np.sum(self.n_distinctlayers)) < self.k):
-                l = np.argmin(self.cum_distinctlayers)
-                y[l].append(F.relu(self.layers[i](y[l][-1])))
-                y_com = torch.cat([y[l][-1] for l in range(len(y))])
+            if ((i//(self.cum_distinctlayers[-1]-1)) < self.k): 
+                # determine which cluster layer belongs to
+                masked_layers = [i < self.cum_distinctlayers[j] for j in range(len(self.cum_distinctlayers))]
+                l = np.min(np.nonzero(masked_layers))
+                if l in y_C:
+                    l_index = y_C.index(l)
+                    y[l_index].append(F.relu(self.layers[i](y[l_index][-1])))
+                if l==(self.k-1):
+                    y_com = torch.cat([y_l[-1] for y_l in y])
             # common encoder layers
             elif i < (self.n_encoderlayers-1):
                 y_com = F.relu(self.layers[i](y_com))
@@ -117,8 +121,7 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, logvar)
         return self.decode(z), mu, logvar
 
-
-model = VAE(input_dim=784,k=1).to(device)
+model = VAE(input_dim=784,k=args.k).to(device)
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
 
@@ -139,9 +142,16 @@ def train(epoch):
     model.train()
     train_loss = 0
     for batch_idx, (data, _) in enumerate(train_loader):
+        # load data
         data = data.to(device)
+        batched_C_train = C_train[(batch_idx*args.batch_size):(batch_idx*args.batch_size+len(data))]
         optimizer.zero_grad()
-        data_C = {'x': data, 'C': C_train[(batch_idx*args.batch_size):(batch_idx*args.batch_size+len(data))]}
+        # sort input obserations according to clusters
+        argsort_batched_C_train = np.argsort(batched_C_train)
+        batched_C_train = batched_C_train[argsort_batched_C_train]
+        data = data[argsort_batched_C_train]
+        data_C = {'x': data, 'C': batched_C_train}
+        # train model
         recon_batch, mu, logvar = model(data_C)
         loss = loss_function(recon_batch, data, mu, logvar)
         loss.backward()
@@ -162,15 +172,21 @@ def test(epoch):
     test_loss = 0
     with torch.no_grad():
         for i, (data, _) in enumerate(test_loader):
+            # load data 
             if i==0:
                 old_len_data = len(data)
             data = data.to(device)
-            data = {'x': data, 'C': C_test[(i*old_len_data):(i*old_len_data+len(data))]}
-            recon_batch, mu, logvar = model(data)
-            test_loss += loss_function(recon_batch, data['x'], mu, logvar).item()
+            # sort data observations according to cluster
+            batched_C_test = C_test[(i*old_len_data):(i*old_len_data+len(data))]
+            argsort_batched_C_test = np.argsort(batched_C_test)
+            batched_C_test = batched_C_test[argsort_batched_C_test]
+            data = data[argsort_batched_C_test]
+            data_C = {'x': data, 'C': batched_C_test}
+            recon_batch, mu, logvar = model(data_C)
+            test_loss += loss_function(recon_batch, data, mu, logvar).item()
             if i == 0:
-                n = min(data['x'].size(0), 8)
-                comparison = torch.cat([data['x'][:n],
+                n = min(data.size(0), 8)
+                comparison = torch.cat([data[:n],
                                       recon_batch.view(args.batch_size, 1, 28, 28)[:n]])
                 save_image(comparison.cpu(),
                          'results/reconstruction_' + str(epoch) + '.png', nrow=n)
